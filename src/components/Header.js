da43ed6103ea8661a4c0b26fad72915a9bbc3268
Reynaldo De Guzman <deguzmanr652@gmail.com>
2024-12-03 01:45:04 +0800

    Use props on Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-const Header = () => {
+import PropTypes from "prop-types";
+
+const Header = ({ title }) => { // Used destructuting for props.title (<any>.title)
   return (
     <header>
-      <h1>Task Tracker</h1>
+      <h1>{title}</h1>
     </header>
-  )
+  );
+};
+
+Header.defaultProps = {
+  title: 'Task Tracker', // If props have no value this will be the default title
 }
 
-export default Header
+Header.propTypes = {
+  title: PropTypes.string.isRequired, // This ensures the prop is a string and required
+}
+
+export default Header;
